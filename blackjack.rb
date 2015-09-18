556d9ff75f0fd18c29fa1c5e1591c382b8c6b519
anne <richardson.ae@gmail.com>
2015-09-18 13:27:18 -0500

    create a deck

--- blackjack.rb
+++ blackjack.rb
@@ -1,1 +1,26 @@
-sample
+# assignment: https://www.gotealeaf.com/lessons/f4d7b093/assignments/1653
+# create the deck
+# define game play
+  # player draws
+  # ask player Stand or Hit
+  # perform Stand or Hit
+  # sum the cards
+
+
+
+# create the deck
+deck_suits = ["clubs", "diamonds", "hearts", "spades"]
+deck_numbers = ["J", "Q", "K", "A"]
+deck = []
+
+(2..10).each do |num| 
+  deck_numbers << num
+end
+
+deck_suits.each do |suit|
+  deck_numbers.each do |num|
+    deck << "#{num} #{suit.capitalize}"
+  end
+end
+
+puts deck
